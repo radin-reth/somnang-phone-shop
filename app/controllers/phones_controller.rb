371e39cf02b8614c8e93cae17484f4a2ef46ed9d
radin-reth <radin.reth@gmail.com>
2015-10-03 09:13:12 +0700

    add page special promotion

--- app/controllers/phones_controller.rb
+++ app/controllers/phones_controller.rb
@@ -5,18 +5,6 @@ class PhonesController < ApplicationController
   def show
   end
 
-  def new
-  end
-
-  def edit
-  end
-
-  def create
-  end
-
-  def update
-  end
-
-  def destroy
+  def special_promotion
   end
 end
