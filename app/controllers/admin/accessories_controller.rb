80e45ca265761ade066f51a8d9b79be3be990afa
radin-reth <radin.reth@gmail.com>
2015-10-17 19:55:43 +0700

    add new resource accessory

--- app/controllers/admin/accessories_controller.rb
+++ app/controllers/admin/accessories_controller.rb
@@ -1,18 +1,26 @@
 class Admin::AccessoriesController < ApplicationController
   layout 'admin'
-  before_action :set_accessory, except: [:index, :new]
+  before_action :set_accessory, except: [:index, :new, :create]
 
   def index
     @accessories = Accessory.all
   end
 
   def new
+    @accessory = Accessory.new
   end
 
   def edit
   end
 
   def create
+    @accessory = Accessory.new(accessory_params)
+
+    if @accessory.save!
+      redirect_to admin_accessories_path, notice: "#{@accessory.name} has been created!"
+    else
+      render @accessory
+    end
   end
 
   def update
@@ -34,6 +42,6 @@ class Admin::AccessoriesController < ApplicationController
   end
 
   def accessory_params
-    
+    params.require(:accessory).permit(:name, :price, :description, :features)
   end
 end
