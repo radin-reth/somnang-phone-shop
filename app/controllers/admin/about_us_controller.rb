5e69b05ef6026b05b0142e8ab8be08d0bbd3dcb6
radin-reth <radin.reth@gmail.com>
2015-11-07 16:12:03 +0700

    feat(UD): update and destroy resource about us

--- app/controllers/admin/about_us_controller.rb
+++ app/controllers/admin/about_us_controller.rb
@@ -1,5 +1,6 @@
 class Admin::AboutUsController < ApplicationController
   layout 'admin'
+  before_action :set_about_us, except: [:index, :new, :create]
 
   def index
     @abouts_us = AboutUs.all
@@ -28,15 +29,27 @@ class Admin::AboutUsController < ApplicationController
   end
 
   def update
+    if @about_us.update_attributes!(about_us_params)
+      redirect_to admin_about_us_path, notice: "#{@about_us.name} has been updated!"
+    else
+      render @about_us
+    end
   end
 
   def show
   end
 
   def destroy
+    @about_us.destroy
+
+    redirect_to admin_about_us_path, notice: "#{@about_us.name} has been deleted!"
   end
 
   private
+
+  def set_about_us
+    @about_us = AboutUs.find(params[:id])
+  end
 
   def about_us_params
     params.require(:about_us).permit(:name, :description)
